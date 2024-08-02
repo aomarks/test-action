19422a650650e8e1965e5fb891e93ed89bd4e406
Al Marks <aomarks@google.com>
2024-08-02 13:40:40 -0700

    foo

--- cleanup.js
+++ cleanup.js
@@ -1,11 +1,5 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+// import { readFileSync } from "fs";
+// import { join } from "path";
 
 console.log("CLEANUP TIME!");
-
-console.log(
-  readFileSync(
-    join(process.env.RUNNER_TEMP, ".wireit-github-cache-info"),
-    "utf8"
-  )
-);
+console.log(process.env);
