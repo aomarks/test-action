1dc40e004b431af6eace48e47f8491616fa8355b
Al Marks <aomarks@google.com>
2024-08-02 13:39:48 -0700

    foo

--- index.js
+++ index.js
@@ -4,56 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { appendFileSync } from "fs";
-import { join } from "path";
+import { fork } from "node:child_process";
 
-const requireEnv = (name) => {
-  const value = process.env[name];
-  if (!value) {
-    throw new Error(`Missing environment variable ${name}`);
+const server = fork("server.js", { detached: true });
+
+server.on("message", (message) => {
+  if (message === "ready") {
+    server.disconnect();
+    server.unref();
   }
-  return value;
-};
-
-// Writing to this file sets environment variables for all subsequent steps in
-// the user's workflow. Reference:
-// https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-environment-variable
-const GITHUB_ENV = requireEnv("GITHUB_ENV");
-
-// The next 2 environment variables are automatically provided to custom
-// workflows like this one, but not to regular "run" steps. By writing these
-// variables to the GITHUB_ENV file, we make them available to the user's "run"
-// steps, and hence to all Wireit invocations.
-
-// URL for the GitHub Actions cache service.
-const ACTIONS_CACHE_URL = requireEnv("ACTIONS_CACHE_URL");
-
-// A secret token for authenticating to the GitHub Actions cache service.
-//
-// This is a JWT which _appears_ to have limited scope, so we believe that the
-// risk of exposing this variable to all workflow steps is low. See
-// https://github.com/actions/toolkit/issues/1053 for more discussion.
-//
-// See also https://github.com/google/wireit/issues/107 for discussion about how
-// we might refactor out GitHub Actions caching support to address this and
-// other issues. In particular, we could potentially start an HTTP server in
-// this action to act as a proxy for this token so that it doesn't need to be
-// shared to all workflow steps.
-const ACTIONS_RUNTIME_TOKEN = requireEnv("ACTIONS_RUNTIME_TOKEN");
-
-appendFileSync(
-  GITHUB_ENV,
-  `
-WIREIT_CACHE=github
-ACTIONS_CACHE_URL=${ACTIONS_CACHE_URL}
-ACTIONS_RUNTIME_TOKEN=${ACTIONS_RUNTIME_TOKEN}
-`
-);
-
-const outputFile = process.env.GITHUB_OUTPUT;
-appendFileSync(outputFile, `sample_output=This is a sample value\n`);
-
-appendFileSync(
-  join(process.env.RUNNER_TEMP, ".wireit-github-cache-info"),
-  JSON.stringify({ ACTIONS_CACHE_URL, ACTIONS_RUNTIME_TOKEN })
-);
+});
---
+++ server.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { writeFileSync } from "node:fs";
+import http from "node:http";
+
+const response = JSON.stringify(
+  {
+    caching: {
+      github: {
+        ACTIONS_CACHE_URL: process.env.ACTIONS_CACHE_URL,
+        ACTIONS_RUNTIME_TOKEN: process.env.ACTIONS_RUNTIME_TOKEN,
+      },
+    },
+  },
+  null,
+  2
+);
+
+const server = http.createServer((req, res) => {
+  if (req.path === "/kill") {
+    console.log("Shutting down");
+    server.close();
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(response);
+  }
+});
+
+const version = (await import("./package.json", { with: { type: "json" } }))
+  .default.version;
+
+const port = 9999;
+
+server.listen(port, "127.0.0.1", () => {
+  process.send("ready");
+  writeFileSync(
+    process.env.GITHUB_ENV,
+    `
+WIREIT_CACHE=github
+WIREIT_GITHUB_SERVER=${JSON.stringify({ version, port })}
+`
+  );
+});
